38e82a7b06916d73c290fb28392c3528f1bbece3
zahidayturan <21060607@stu.omu.edu.tr>
2024-07-24 10:44:57 +0300

    add project files
    config is ignore

---
+++ src/main/java/com/example/courseup/CourseUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.courseup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseUpApplication.class, args);
+    }
+
+}
