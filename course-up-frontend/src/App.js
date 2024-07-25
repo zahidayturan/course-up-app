253b0798ae72d3467629f4bd5c41984a65916a1f
zahidayturan <21060607@stu.omu.edu.tr>
2024-07-25 12:17:35 +0300

    add frontend files and edit backend root directory
    react

---
+++ course-up-frontend/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="App">
+      <header className="App-header">
+          <p>
+              Merhaba Baba, Nasılsınız
+          </p>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
