ed3cc66c660650879d5f061674efd3affe05d437
zahidayturan <21060607@stu.omu.edu.tr>
2024-07-25 13:27:15 +0300

    cors.allowed-origins
    in config.properties

--- course-up-backend/src/main/java/com/example/courseup/config/SecurityConfig.java
+++ course-up-backend/src/main/java/com/example/courseup/config/SecurityConfig.java
@@ -1,15 +1,20 @@
 package com.example.courseup.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig implements WebMvcConfigurer {
 
+    @Value("${cors.allowed-origins}")
+    private String[] allowedOrigins;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -19,5 +24,12 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins) //Local or On Your Network:
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
