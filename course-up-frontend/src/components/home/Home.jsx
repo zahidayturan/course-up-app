f9d85b7f79af8fbbbd57c2929d52cc569fbe5c05
zahidayturan <21060607@stu.omu.edu.tr>
2024-07-29 16:16:13 +0300

    header auth operation

--- course-up-frontend/src/components/home/Home.jsx
+++ course-up-frontend/src/components/home/Home.jsx
@@ -2,48 +2,11 @@ import React from 'react';
 import Header from './components/Header';
 import '../../assets/css/Main.module.css';
 import '../../assets/css/Text.module.css';
-import {useNavigate} from "react-router-dom";
-import axios from "axios";
-import Endpoints from "../../constants/Endpoints";
 
 const Home = () => {
-    const [user, setUser] = React.useState(null);
-    const navigate = useNavigate();
-
-
-    React.useEffect(() => {
-        const storedUser = localStorage.getItem('user');
-        if (storedUser) {
-            setUser(JSON.parse(storedUser));
-        } else {
-            console.log('No user data found in localStorage');
-        }
-    }, []);
-
-    const handleLogout = async () => {
-        try {
-            await axios.get(Endpoints.LOGOUT);
-            localStorage.removeItem('user');
-            navigate('/login');
-        } catch (error) {
-            console.error("Logout failed:", error);
-        }
-    };
-
-
     return (
         <div>
             <Header />
-            <br/>
-            {user ? (
-                <div>
-                    <p>Welcome, {user.name}!</p>
-                    <p>Email: {user.email}</p>
-                    <button onClick={handleLogout}>Logout</button>
-                </div>
-            ) : (
-                <p>Loading...</p>
-            )}
         </div>
     );
 };
