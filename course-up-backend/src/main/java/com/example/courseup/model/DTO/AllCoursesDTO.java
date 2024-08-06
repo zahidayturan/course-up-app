ae5253c17bd826b03d54a35be528818589167a0e
zahidayturan <21060607@stu.omu.edu.tr>
2024-08-06 20:43:37 +0300

    course detail api endpoint

--- course-up-backend/src/main/java/com/example/courseup/model/DTO/AllCoursesDTO.java
+++ course-up-backend/src/main/java/com/example/courseup/model/DTO/AllCoursesDTO.java
@@ -12,6 +12,8 @@ public class AllCoursesDTO {
     private String name;
     private String instructor;
     private String description;
+
+    private String category;
     private Integer duration;
     private Integer stage;
     private Integer students;
@@ -28,6 +30,7 @@ public class AllCoursesDTO {
         this.name = course.getName();
         this.instructor = course.getTeacher().getUser().getName() + " " + course.getTeacher().getUser().getSurname();
         this.description = course.getDescription();
+        this.category = course.getCategory();
         this.duration = course.getTotalDuration();
         this.stage = course.getTotalStages();
         this.students = students;
