e8d0bc9d805a389dbb6982fbed01102a40071d71
zahidayturan <21060607@stu.omu.edu.tr>
2024-08-10 22:54:15 +0300

    basket page init

---
+++ course-up-frontend/src/components/basket/Basket.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../home/components/Header';
+
+const Basket = () => {
+    return (
+        <div>
+            <Header />
+        </div>
+    );
+};
+
+export default Basket;
